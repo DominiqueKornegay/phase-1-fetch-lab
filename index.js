73a0b875868d668d1b53c7af5c3b1d3b53209818
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-07-14 16:39:17 -0400

    commit tree 63e1514d6ffdd6b6e9297b34158788ed41146d7a

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch ("https://anapioficeandfire.com/api/books")
+  .then (function response (res) {
+    return res.json()
+  }) 
+  .then(function manipulation (JSON) {
+    renderBooks(JSON)
+  });
   
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  let main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
+    let h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
